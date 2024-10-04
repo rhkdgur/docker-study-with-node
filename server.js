081fe92b0eba91cdc27731d926a69956e7775ecb
rhkdgur <rhkdgur195@gmail.com>
2024-10-04 19:12:40 +0900

    redis를 포함하여 docker compose 작성해보기

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const express = require("express");
+const redis = require("redis");
+
+//redis client 생성
+const client = redis.createClient({
+    socket: {
+        host: 'redis-server',
+        port: '6379'
+    }
+})
 
 const port = 8080;
 
 //APP
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send("Hello world");
-});
+app.get('/', async (req, res) => {
+
+await client.connect();
+
+let number = await client.get('number');
+
+if (number === null) {
+
+number = 0;
+
+}
+
+console.log('Number: ' + number);
+
+res.send("숫자가 1씩 올라갑니다. 숫자: " + number)
+
+await client.set("number", parseInt(number) + 1)
+
+await client.disconnect();;;
+
+})
 
 app.listen(port);
-console.log("Server is Running")
+console.log("Server is Running");
